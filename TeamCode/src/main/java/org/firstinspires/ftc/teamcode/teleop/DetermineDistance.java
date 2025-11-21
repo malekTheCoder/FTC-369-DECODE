fef162324ce428d452dc6caf1356314f9f03fb14
EC-0 <ethanchen13001@gmail.com>
2025-11-21 16:19:47 -0500

    pipeline switch for red and blue

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/DetermineDistance.java
+++
@@ -1,6 +0,0 @@
-package org.firstinspires.ftc.teamcode.teleop;
-import org.firstinspires.ftc.vision.VisionPortal;
-import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
-
-public class DetermineDistance {
-}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/RedTeleopV2Bot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/RedTeleopV2Bot.java
@@ -1,43 +1,27 @@
 package org.firstinspires.ftc.teamcode.teleop;
 
-import android.util.Size;
-
-import com.ThermalEquilibrium.homeostasis.Controllers.Feedback.BasicPID;
 import com.ThermalEquilibrium.homeostasis.Controllers.Feedback.PIDEx;
-import com.ThermalEquilibrium.homeostasis.Parameters.PIDCoefficients;
 import com.ThermalEquilibrium.homeostasis.Parameters.PIDCoefficientsEx;
+import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.hardware.Servo;
-import com.acmerobotics.dashboard.FtcDashboard;
-import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 
-import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
-import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.ExposureControl;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
 import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
-import org.firstinspires.ftc.robotcore.internal.hardware.android.DragonboardIndicatorLED;
-import org.firstinspires.ftc.vision.VisionPortal;
-import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
-import org.firstinspires.ftc.vision.apriltag.AprilTagGameDatabase;
-import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
-
-import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 @Config
 @TeleOp(name = " V2 Bot Teleop")
-public class TeleopV2Bot extends OpMode {
+public class RedTeleopV2Bot extends OpMode {
     private FtcDashboard dashboard;
 
     private Limelight3A limelight;
@@ -129,6 +113,7 @@ public class TeleopV2Bot extends OpMode {
 
         dashboard = FtcDashboard.getInstance();
         limelight.start();
+        limelight.pipelineSwitch(1);
 
         telemetry.addLine("Hardware Initialized!");
     }
