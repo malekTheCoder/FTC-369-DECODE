ab84e3ae217748aaa9dfcdebff9114956953217e
Malek Swilam <malekgswilam@gmail.com>
2025-11-25 16:53:28 -0500

    added megatag 1

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/OneBotClose.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/OneBotClose.java
@@ -15,28 +15,34 @@ public class OneBotClose {
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 .setColorScheme(new ColorSchemeRedDark()) // set our bot to be red
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(60, 50, Math.toRadians(180), Math.toRadians(180), 15)
+                .setConstraints(80, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .build();
 
-        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-57, -44, Math.toRadians(233)))
-                .strafeToLinearHeading(new Vector2d(-44,-28),Math.toRadians(240)) // go to shoot preload
-
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-44, -28, Math.toRadians(240)))
+                        .waitSeconds(2.5)
                 .strafeToLinearHeading(new Vector2d(-12,-27), Math.toRadians(270)) // go to get first set of artifacts
                 .strafeToLinearHeading(new Vector2d(-12,-50), Math.toRadians(270)) // drive into first set of artifacts
 
                 .strafeToLinearHeading(new Vector2d(-44,-28),Math.toRadians(240)) // go to shoot first batch
+                        .waitSeconds(2.5)
 
                 .strafeToLinearHeading(new Vector2d(12,-30), Math.toRadians(270)) // go to second set of artifacts
                 .strafeToLinearHeading(new Vector2d(12,-50), Math.toRadians(270)) // drive into second set of artifacts
 
                 .turnTo(Math.toRadians(180))
                 .strafeToLinearHeading(new Vector2d(-2,-58), Math.toRadians(180)) // push classifier gate
+                        .waitSeconds(3)
 
-                .strafeToLinearHeading(new Vector2d(-44,-28),Math.toRadians(240)) // go to shoot second batch
+                .strafeToLinearHeading(new Vector2d(-44,-28),Math.toRadians(240)) // go to shoot second batch\
+                        .waitSeconds(2.5)
 
                 .strafeToLinearHeading(new Vector2d(35,-30), Math.toRadians(270)) // go to third set of artifacts
+
+
+
                 .strafeToLinearHeading(new Vector2d(35,-50), Math.toRadians(270)) // drive into third set of artifacts
                 .strafeToLinearHeading(new Vector2d(-44,-28),Math.toRadians(240)) // go to shoot
+                        .waitSeconds(2.5)
 
 
 
