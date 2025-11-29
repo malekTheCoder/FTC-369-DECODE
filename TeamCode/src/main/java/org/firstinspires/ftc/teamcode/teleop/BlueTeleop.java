d276b6529572265beb839700185ac10183507b94
Malek Swilam <malekgswilam@gmail.com>
2025-11-29 16:16:58 -0500

    servo kicker position

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/BlueTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/BlueTeleop.java
@@ -21,7 +21,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 
 @Config
 @TeleOp(name = " TELEOP BLUE")
-public class TeleopBlue extends OpMode {
+public class BlueTeleop extends OpMode {
     private FtcDashboard dashboard;
 
     private Limelight3A limelight;
@@ -85,11 +85,13 @@ public class TeleopBlue extends OpMode {
     private double tx;
 
     private double flyMultiplier;
+    private double flyAddVelocity;
+    private boolean manualFlywheelControl = false;
     private double intakeMultiplier;
 
     private boolean kickerUp;
-    private double kickerUpPosition = 0;
-    private double kickerDownPosition = 1;
+    private double kickerUpPosition = 0.5;
+    private double kickerDownPosition = 0;
 
     private double verticalTranslation;
     private double flyTx;
@@ -107,11 +109,14 @@ public class TeleopBlue extends OpMode {
         belt = hardwareMap.get(DcMotor.class, "belt");
         rgbLight = hardwareMap.get(Servo.class, "rgb");
 
+        kickerUp = false;
+
         turnPIDCoeffs = new PIDCoefficientsEx(Kp, Ki, Kd, integralSumMax, stabilityThreshold, lowPassGain);
         turnPID = new PIDEx(turnPIDCoeffs);
 
         intakeMultiplier = 0.6;
         verticalTranslation = 75;
+        flyAddVelocity = 2110;
 
         dashboard = FtcDashboard.getInstance();
         limelight.start();
@@ -208,7 +213,7 @@ public class TeleopBlue extends OpMode {
 
             if (kickerUp){
                 kickerUp = false;
-            } else {
+            } else if(!kickerUp){
                 kickerUp = true;
             }
 
@@ -228,145 +233,160 @@ public class TeleopBlue extends OpMode {
                 flyMultiplier = 1;
             }
         }
-        fly.setVelocity(targetVel* flyMultiplier); // ticks per second (negative allowed)
-    }
+        if (gamepad2.bWasPressed()){
+            if (!manualFlywheelControl){
+                manualFlywheelControl = true;
+            } else if (manualFlywheelControl){
+                manualFlywheelControl = false;
+            }
+        }
+
+        if (manualFlywheelControl){
+            fly.setVelocity(flyAddVelocity);
+        } else if (!manualFlywheelControl) {
+            fly.setVelocity(targetVel * flyMultiplier); // ticks per second (negative allowed)
+
+        }
+
+
+}
 
 //    private double shooterModel (double distanceInches){
 //        return 10.80327*distanceInches+(1574.00755-verticalTranslation); // add regression here to return the velocity needed given the distance
 //    }
 
-    private double shooterModel (double distanceInches){
-            return 8.78571*distanceInches+1641.42857; // add regression here to return the velocity needed given the distance
+private double shooterModel (double distanceInches){
+    return 8.78571*distanceInches+1641.42857; // add regression here to return the velocity needed given the distance
+}
+
+private void handleRGB(){
+    if((Math.abs(targetVel-actualVel)<80 && Math.abs(turnCommand)<.1)) {
+        rgbLight.setPosition(0.5);
+    }
+    else{
+        rgbLight.setPosition(.28);
+    }
+}
+
+private void updateLimelightInfo() {
+    orientation = imu.getRobotYawPitchRollAngles();
+    limelight.updateRobotOrientation(orientation.getYaw());
+    llResult = limelight.getLatestResult();
+    if (llResult != null && llResult.isValid()) {
+        ty = llResult.getTy();
+        tx = llResult.getTx();
+    }else {
+        ty = 0;
+        tx = 0;
     }
 
-    private void handleRGB(){
-        if((Math.abs(targetVel-actualVel)<80 && Math.abs(turnCommand)<.1)) {
-            rgbLight.setPosition(0.5);
-        }
-        else{
-            rgbLight.setPosition(.28);
-        }
-    }
-
-    private void updateLimelightInfo() {
-        orientation = imu.getRobotYawPitchRollAngles();
-        limelight.updateRobotOrientation(orientation.getYaw());
-        llResult = limelight.getLatestResult();
-        if (llResult != null && llResult.isValid()) {
-            ty = llResult.getTy();
-            tx = llResult.getTx();
-        }else {
-            ty = 0;
-            tx = 0;
-        }
-
-        targetOffsetAngle_Vertical = ty;
-        angleToGoalDegrees = limelightMountAngleDegrees + targetOffsetAngle_Vertical;
-        angleToGoalRadians = angleToGoalDegrees * (3.14159 / 180.0);
-        distanceFromLimelightToGoalInches = (goalHeightInches - limelightLensHeightInches) / Math.tan(angleToGoalRadians);
-    }
+    targetOffsetAngle_Vertical = ty;
+    angleToGoalDegrees = limelightMountAngleDegrees + targetOffsetAngle_Vertical;
+    angleToGoalRadians = angleToGoalDegrees * (3.14159 / 180.0);
+    distanceFromLimelightToGoalInches = (goalHeightInches - limelightLensHeightInches) / Math.tan(angleToGoalRadians);
+}
 
 
-    private void handleBelt() {
-        belt.setPower(beltPowerScale * gamepad2.left_stick_y);
-    }
+private void handleBelt() {
+    belt.setPower(beltPowerScale * gamepad2.left_stick_y);
+}
 
-    private void handleIntake(){
-        intake.setPower(gamepad2.left_trigger * intakeMultiplier);
-    }
-    private void handleDrivetrain() {
+private void handleIntake(){
+    intake.setPower(gamepad2.left_trigger * intakeMultiplier);
+}
+private void handleDrivetrain() {
 
-        if (gamepad1.x){
-            imu.resetYaw();
-        }
-
-        // drivetrain code to get inputs from controller and call the drive method w/ parameters
-        double rightStickX = gamepad1.right_stick_x;
-        if (Math.abs(rightStickX) < 0.05) rightStickX = 0;
-
-
-        double x = gamepad1.left_stick_x;
-        double y = -gamepad1.left_stick_y;
-        double rx = (gamepad1.right_trigger - gamepad1.left_trigger + (rightStickX * slowRotationScale));
-
-        drive(y, x, rx);
-    }
-
-    private void aim() {
-        if(aiming) {
-            currentHeading = botHeadingIMU;
-            turnError = AngleUnit.normalizeRadians(desiredHeading - currentHeading);
-            turnCommand = turnPID.calculate(0.0, turnError);
-
-            if (turnCommand > 1) turnCommand = 1;
-            if (turnCommand < -1) turnCommand = -1;
-
-            drive(0, 0, turnCommand);
-        }
-    }
-
-    public void drive(double forward, double strafe, double rotate){
-        // Always field-centric: rotate the joystick vector by -heading
-        double theta = Math.atan2(forward, strafe);
-        double r = Math.hypot(strafe, forward);
-
-        double heading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
-        double rotatedTheta = AngleUnit.normalizeRadians(theta - heading);
-
-        double f = r * Math.sin(rotatedTheta);
-        double s = r * Math.cos(rotatedTheta);
-
-        // Mecanum kinematics
-        double frontLeftPower = f + s + rotate;
-        double backLeftPower = f - s + rotate;
-        double frontRightPower = f - s - rotate;
-        double backRightPower = f + s - rotate;
-
-        // Normalize so no value exceeds 1.0
-        double max = Math.max(1.0, Math.max(Math.abs(frontLeftPower),
-                Math.max(Math.abs(backLeftPower),
-                        Math.max(Math.abs(frontRightPower),
-                                Math.abs(backRightPower)))));
-
-        frontLeft.setPower(frontLeftPower / max);
-        backLeft.setPower(backLeftPower / max);
-        frontRight.setPower(frontRightPower / max);
-        backRight.setPower(backRightPower / max);
-
-
-    }
-
-    private void initDriveAndIMU() {
-        frontRight = hardwareMap.get(DcMotorEx.class, "frontRight");
-        frontLeft = hardwareMap.get(DcMotorEx.class, "frontLeft");
-        backLeft = hardwareMap.get(DcMotorEx.class, "backLeft");
-        backRight = hardwareMap.get(DcMotorEx.class, "backRight");
-
-        frontRight.setDirection(DcMotor.Direction.REVERSE);
-        backRight.setDirection(DcMotor.Direction.REVERSE);
-
-        frontLeft.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
-        backLeft.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
-        frontRight.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
-        backRight.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
-
-        imu = hardwareMap.get(IMU.class, "imu");
-        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
-                RevHubOrientationOnRobot.LogoFacingDirection.RIGHT,
-                RevHubOrientationOnRobot.UsbFacingDirection.UP));
-
-        imu.initialize(parameters);
-
+    if (gamepad1.x){
         imu.resetYaw();
-        botHeadingIMU = imu.getRobotYawPitchRollAngles().getYaw();
     }
 
+    // drivetrain code to get inputs from controller and call the drive method w/ parameters
+    double rightStickX = gamepad1.right_stick_x;
+    if (Math.abs(rightStickX) < 0.05) rightStickX = 0;
 
-    private void initFlyWheel() {
-        fly = hardwareMap.get(DcMotorEx.class, "fly");
-        fly.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        fly.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        fly.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+    double x = gamepad1.left_stick_x;
+    double y = -gamepad1.left_stick_y;
+    double rx = (gamepad1.right_trigger - gamepad1.left_trigger + (rightStickX * slowRotationScale));
+
+    drive(y, x, rx);
+}
+
+private void aim() {
+    if(aiming) {
+        currentHeading = botHeadingIMU;
+        turnError = AngleUnit.normalizeRadians(desiredHeading - currentHeading);
+        turnCommand = turnPID.calculate(0.0, turnError);
+
+        if (turnCommand > 1) turnCommand = 1;
+        if (turnCommand < -1) turnCommand = -1;
+
+        drive(0, 0, turnCommand);
     }
+}
+
+public void drive(double forward, double strafe, double rotate){
+    // Always field-centric: rotate the joystick vector by -heading
+    double theta = Math.atan2(forward, strafe);
+    double r = Math.hypot(strafe, forward);
+
+    double heading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+    double rotatedTheta = AngleUnit.normalizeRadians(theta - heading);
+
+    double f = r * Math.sin(rotatedTheta);
+    double s = r * Math.cos(rotatedTheta);
+
+    // Mecanum kinematics
+    double frontLeftPower = f + s + rotate;
+    double backLeftPower = f - s + rotate;
+    double frontRightPower = f - s - rotate;
+    double backRightPower = f + s - rotate;
+
+    // Normalize so no value exceeds 1.0
+    double max = Math.max(1.0, Math.max(Math.abs(frontLeftPower),
+            Math.max(Math.abs(backLeftPower),
+                    Math.max(Math.abs(frontRightPower),
+                            Math.abs(backRightPower)))));
+
+    frontLeft.setPower(frontLeftPower / max);
+    backLeft.setPower(backLeftPower / max);
+    frontRight.setPower(frontRightPower / max);
+    backRight.setPower(backRightPower / max);
+
+
+}
+
+private void initDriveAndIMU() {
+    frontRight = hardwareMap.get(DcMotorEx.class, "frontRight");
+    frontLeft = hardwareMap.get(DcMotorEx.class, "frontLeft");
+    backLeft = hardwareMap.get(DcMotorEx.class, "backLeft");
+    backRight = hardwareMap.get(DcMotorEx.class, "backRight");
+
+    frontRight.setDirection(DcMotor.Direction.REVERSE);
+    backRight.setDirection(DcMotor.Direction.REVERSE);
+
+    frontLeft.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+    backLeft.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+    frontRight.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+    backRight.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+
+    imu = hardwareMap.get(IMU.class, "imu");
+    IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+            RevHubOrientationOnRobot.LogoFacingDirection.RIGHT,
+            RevHubOrientationOnRobot.UsbFacingDirection.UP));
+
+    imu.initialize(parameters);
+
+    imu.resetYaw();
+    botHeadingIMU = imu.getRobotYawPitchRollAngles().getYaw();
+}
+
+
+private void initFlyWheel() {
+    fly = hardwareMap.get(DcMotorEx.class, "fly");
+    fly.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    fly.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    fly.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+}
 
 }
