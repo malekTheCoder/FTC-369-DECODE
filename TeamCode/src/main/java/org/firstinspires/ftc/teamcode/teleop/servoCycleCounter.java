811adee8c1aefa259e79f27efb265f15674ec97e
EC-0 <ethanchen13001@gmail.com>
2025-12-21 15:13:40 -0500

    working printing to file with cycle counts on servos

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/servoCycleCounter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/servoCycleCounter.java
@@ -10,6 +10,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.acmerobotics.dashboard.FtcDashboard;
@@ -19,11 +20,17 @@ import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
 import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 
-@TeleOp(name = " V2 Bot Teleop")
+import android.os.Environment;
+
+@TeleOp(name = "Servo counter")
 public class servoCycleCounter extends OpMode {
     private FtcDashboard dashboard;
 
@@ -127,121 +134,60 @@ public class servoCycleCounter extends OpMode {
     @Override
     public void loop() {
         updateLimelightInfo();
-        handleFlywheel();
-        handleIntake();
-        handleBelt();
-        handleKicker();
-        handleRGB();
-        limelightOffest();
 
-        botHeadingIMU = AngleUnit.normalizeRadians(imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS));
-
-
-        if (gamepad1.aWasPressed()){
-            // if valid detection
-            if (llResult != null && llResult.isValid()){
-                aiming = true;
-                botHeadingAtCapture = botHeadingIMU;
-                desiredHeading = AngleUnit.normalizeRadians(botHeadingAtCapture - AngleUnit.normalizeRadians(Math.toRadians(flyTx)));
-                // minus 4 from the tx becasue the camera is to the left of the bot, centering the bot gets aroudn 4 tx
-            }
-        } else if (gamepad1.aWasReleased()){
-            aiming = false;
+        if(gamepad1.xWasPressed()){
+            writeToFile(Environment.getExternalStorageDirectory().getPath()+"/ServoCycleCount.txt",1, "servoName: ");
+            //telemetry.addData("filepath/name", Environment.getExternalStorageDirectory().getPath()+"/ServoCycleCount.txt");
+            telemetry.addLine("printed to file");
         }
-
-
-        if (gamepad1.a){
-            aim();
-        } else {
-            handleDrivetrain();
+        if(gamepad1.bWasPressed()){
+            writeToFile(Environment.getExternalStorageDirectory().getPath()+"/ServoCycleCount.txt",-1, "servoName: ");
         }
-
-
-        telemetry.addData("Target (tps)", targetVel);
-        telemetry.addData("Actual (tps)", fly.getVelocity());
-
-        if (llResult != null && llResult.isValid()){
-            Pose3D botPose = llResult.getBotpose_MT2();
-            telemetry.addData("Tx", llResult.getTx());
-            telemetry.addData("Ty", llResult.getTy());
-            telemetry.addData("Ta", llResult.getTa());
-            telemetry.addData("Turn Error", turnError);
-            telemetry.addData("Bot pose", botPose.toString());
-            telemetry.addData("Yaw", botPose.getOrientation().getYaw());
-            telemetry.addData("Distance", distanceFromLimelightToGoalInches);
-            telemetry.addData("Fly Tx", flyTx);
-            telemetry.addData("Fly Distance", d2);
-        }
-
-
-        TelemetryPacket packet = new TelemetryPacket();
-        packet.put("targetVel", targetVel);
-        packet.put("actualVel", actualVel);  // or fly.getVelocity() if you prefer
-        dashboard.sendTelemetryPacket(packet);
-
         telemetry.update();
     }
 
 
-    private void limelightOffest(){
-        //Use law of cos with SAS to find the third side (d2)
-        //Use law of sin to find the 2nd base angle of triangle
-        //90-(2nd base angle) to find the error between fly heading and tag heading
-        double B=Math.abs(90-tx);
-        double x = distanceFromLLTOFly;
-        double d = distanceFromLimelightToGoalInches;
-        d2 = Math.sqrt(Math.pow(x, 2) + Math.pow(d, 2)-2*x*d*Math.cos(Math.toRadians(B))); // d2 is the distance to the april tag from the fly wheel, formula: sqrt(a^2+c^2-2ac cos(B)
-        flyTx = 90 - Math.toDegrees(Math.asin(d * (Math.sin(Math.toRadians(B))/d2))); //sin-1(d(Sin(B)/d2
-    }
-    private void handleKicker(){
-        if (gamepad2.leftBumperWasPressed()){
-            if (kickerUp){
-                kicker.setPosition(kickerDownPosition);
-            } else if (!kickerUp){
-                kicker.setPosition(kickerUpPosition);
+
+
+    public void writeToFile(String filename, int data, String label) {
+        File file = new File(Environment.getExternalStorageDirectory()+"/ServoCycleCount.txt");
+        String[] readData = null;
+        try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+            String line;
+            int readDataInt;
+            // Read each line until the end of the file (returns null)
+            while ((line = reader.readLine()) != null) {
+                readData = line.split(" ");
+                readDataInt = Integer.parseInt(readData[1]);
+                readData[1]= String.valueOf(readDataInt+=data);
+                telemetry.addData("readData", readData[1]);
+                telemetry.addLine(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        //PRINT to file
+        try (PrintWriter out = new PrintWriter(new FileWriter(filename))) {
+            if(readData!=null) {
+                //out.println("ServoCycleCounter");
+                out.println(label + readData[1]);
+            }
+            if(readData == null){
+                out.println(label + "0");
             }
 
-            if (kickerUp){
-                kickerUp = false;
+            if (file.createNewFile()) {
+                telemetry.addLine("File created successfully: " + file.getAbsolutePath());
             } else {
-                kickerUp = true;
+                telemetry.addLine("File already exists: " + file.getAbsolutePath());
             }
-
-
+        } catch (IOException e) {
+            telemetry.addLine("Error writing to file: " + e.getMessage());
         }
     }
 
-    private void handleFlywheel() {
-        actualVel = fly.getVelocity();
-        targetVel = shooterModel(distanceFromLimelightToGoalInches);
-
-        if (gamepad2.aWasPressed()){
-            if (flyMultiplier == 1){
-                flyMultiplier = 0;
-            }
-            else{
-                flyMultiplier = 1;
-            }
-        }
-        fly.setVelocity(targetVel* flyMultiplier); // ticks per second (negative allowed)
-    }
-
-//    private double shooterModel (double distanceInches){
-//        return 10.80327*distanceInches+(1574.00755-verticalTranslation); // add regression here to return the velocity needed given the distance
-//    }
-
-    private double shooterModel (double distanceInches){
-        return 8.78571*distanceInches+1641.42857; // add regression here to return the velocity needed given the distance
-    }
-
-    private void handleRGB(){
-        if((Math.abs(targetVel-actualVel)<80 && Math.abs(turnCommand)<.1)) {
-            rgbLight.setPosition(0.5);
-        }
-        else{
-            rgbLight.setPosition(.28);
-        }
-    }
 
     private void updateLimelightInfo() {
         orientation = imu.getRobotYawPitchRollAngles();
@@ -261,75 +207,6 @@ public class servoCycleCounter extends OpMode {
         distanceFromLimelightToGoalInches = (goalHeightInches - limelightLensHeightInches) / Math.tan(angleToGoalRadians);
     }
 
-
-    private void handleBelt() {
-        belt.setPower(beltPowerScale * gamepad2.left_stick_y);
-    }
-
-    private void handleIntake(){
-        intake.setPower(gamepad2.left_trigger * intakeMultiplier);
-    }
-    private void handleDrivetrain() {
-
-        if (gamepad1.x){
-            imu.resetYaw();
-        }
-
-        // drivetrain code to get inputs from controller and call the drive method w/ parameters
-        double rightStickX = gamepad1.right_stick_x;
-        if (Math.abs(rightStickX) < 0.05) rightStickX = 0;
-
-
-        double x = gamepad1.left_stick_x;
-        double y = -gamepad1.left_stick_y;
-        double rx = (gamepad1.right_trigger - gamepad1.left_trigger + (rightStickX * slowRotationScale));
-
-        drive(y, x, rx);
-    }
-
-    private void aim() {
-        if(aiming) {
-            currentHeading = botHeadingIMU;
-            turnError = AngleUnit.normalizeRadians(desiredHeading - currentHeading);
-            turnCommand = turnPID.calculate(0.0, turnError);
-
-            if (turnCommand > 1) turnCommand = 1;
-            if (turnCommand < -1) turnCommand = -1;
-
-            drive(0, 0, turnCommand);
-        }
-    }
-
-    public void drive(double forward, double strafe, double rotate){
-        // Always field-centric: rotate the joystick vector by -heading
-        double theta = Math.atan2(forward, strafe);
-        double r = Math.hypot(strafe, forward);
-
-        double heading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
-        double rotatedTheta = AngleUnit.normalizeRadians(theta - heading);
-
-        double f = r * Math.sin(rotatedTheta);
-        double s = r * Math.cos(rotatedTheta);
-
-        // Mecanum kinematics
-        double frontLeftPower = f + s + rotate;
-        double backLeftPower = f - s + rotate;
-        double frontRightPower = f - s - rotate;
-        double backRightPower = f + s - rotate;
-
-        // Normalize so no value exceeds 1.0
-        double max = Math.max(1.0, Math.max(Math.abs(frontLeftPower),
-                Math.max(Math.abs(backLeftPower),
-                        Math.max(Math.abs(frontRightPower),
-                                Math.abs(backRightPower)))));
-
-        frontLeft.setPower(frontLeftPower / max);
-        backLeft.setPower(backLeftPower / max);
-        frontRight.setPower(frontRightPower / max);
-        backRight.setPower(backRightPower / max);
-
-
-    }
 
     private void initDriveAndIMU() {
         frontRight = hardwareMap.get(DcMotorEx.class, "frontRight");
@@ -362,16 +239,6 @@ public class servoCycleCounter extends OpMode {
         fly.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         fly.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         fly.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-    }
-
-    public static void writeToFile(String filename, String data) {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter("/FIRST/settings/" + filename, true))) { // 'true' for appending
-            writer.write(data);
-            writer.newLine(); // Add a new line
-
-        } catch (IOException e) {
-            System.err.println("Error writing to file: " + e.getMessage());
-        }
     }
 }
 
