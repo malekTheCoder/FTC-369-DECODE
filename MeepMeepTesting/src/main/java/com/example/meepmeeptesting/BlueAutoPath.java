99c09ff904bca9eefc11a3470a4262703ae21115
EC-0 <ethanchen13001@gmail.com>
2026-01-07 17:09:37 -0500

    Pathing for new bot

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/BlueAutoPath.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/BlueAutoPath.java
@@ -3,12 +3,11 @@ package com.example.meepmeeptesting;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
-import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeBlueDark;
 import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeRedDark;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
-public class OneBotAutoPath {
+public class BlueAutoPath {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(500);
 
@@ -18,13 +17,13 @@ public class OneBotAutoPath {
                 .setConstraints(60, 50, Math.toRadians(180), Math.toRadians(180), 15)
                 .build();
 
-        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(61, -10, Math.toRadians(180)))
-                .strafeToLinearHeading(new Vector2d(53,-12), Math.toRadians(205)) // position to shoot zero batch
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(61, -10, Math.toRadians(90)))
+                .strafeToLinearHeading(new Vector2d(53,-12), Math.toRadians(270)) // position to shoot zero batch
                 .waitSeconds(3)
 
                 .strafeToLinearHeading(new Vector2d(35,-30), Math.toRadians(270)) // go to first set of artifacts
                 .strafeToLinearHeading(new Vector2d(35,-50), Math.toRadians(270)) // drive into first set of artifacts
-                .strafeToLinearHeading(new Vector2d(55,-15), Math.toRadians(195)) // go back after grabbing first set of artifacts to shoot
+                .strafeToLinearHeading(new Vector2d(55,-15), Math.toRadians(270)) // go back after grabbing first set of artifacts to shoot
                 .waitSeconds(3)
 
                 .strafeToLinearHeading(new Vector2d(12,-30), Math.toRadians(270)) // go to second set of artifacts
@@ -32,13 +31,13 @@ public class OneBotAutoPath {
 
 //
 
-                .strafeToLinearHeading(new Vector2d(55,-15), Math.toRadians(195)) // go back after grabbing second set of artifacts to shoot
+                .strafeToLinearHeading(new Vector2d(55,-15), Math.toRadians(270)) // go back after grabbing second set of artifacts to shoot
                         .waitSeconds(3)
 
                 .strafeToLinearHeading(new Vector2d(38,-62), Math.toRadians(-10)) // wall set
                 .strafeToLinearHeading(new Vector2d(60,-62), Math.toRadians(-10)) // drive in
 
-                .strafeToLinearHeading(new Vector2d(55,-15), Math.toRadians(195)) // go back after grabbing wall set
+                .strafeToLinearHeading(new Vector2d(55,-15), Math.toRadians(270)) // go back after grabbing wall set
                         .waitSeconds(3)
 
 
---
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/BlueAutoPathClose.java
@@ -0,0 +1,63 @@
+package com.example.meepmeeptesting;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeRedDark;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+public class BlueAutoPathClose {
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(500);
+
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                .setColorScheme(new ColorSchemeRedDark()) // set our bot to be red
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 50, Math.toRadians(180), Math.toRadians(180), 15)
+                .build();
+
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-50, -49, Math.toRadians(234)))
+                .strafeToLinearHeading(new Vector2d(-12,-20), Math.toRadians(270)) // position to shoot zero batch
+                .waitSeconds(3)
+
+                .strafeToLinearHeading(new Vector2d(-12,-30), Math.toRadians(270)) // go to first set of artifacts
+                .strafeToLinearHeading(new Vector2d(-12,-50), Math.toRadians(270)) // drive into first set of artifacts
+                .strafeToLinearHeading(new Vector2d(-12,-20), Math.toRadians(270)) // go back after grabbing first set of artifacts to shoot
+                .waitSeconds(3)
+
+                .strafeToLinearHeading(new Vector2d(12,-30), Math.toRadians(270)) // go to second set of artifacts
+                .strafeToLinearHeading(new Vector2d(12,-50), Math.toRadians(270)) // drive into second set of artifacts
+
+//
+
+                .strafeToLinearHeading(new Vector2d(-12,-20), Math.toRadians(270)) // go back after grabbing second set of artifacts to shoot
+                .waitSeconds(3)
+
+                .strafeToLinearHeading(new Vector2d(35,-30), Math.toRadians(270)) // go to first set of artifacts
+                .strafeToLinearHeading(new Vector2d(35,-50), Math.toRadians(270)) // drive into first set of artifacts
+                .strafeToLinearHeading(new Vector2d(-12,-20), Math.toRadians(270)) // go back after grabbing first set of artifacts to shoot
+                .waitSeconds(3)
+
+
+//                .strafeToLinearHeading(new Vector2d(38,-62), Math.toRadians(-10)) // wall set
+//                .strafeToLinearHeading(new Vector2d(60,-62), Math.toRadians(-10)) // drive in
+//
+//                .strafeToLinearHeading(new Vector2d(-12,-15), Math.toRadians(270)) // go back after grabbing wall set
+//                .waitSeconds(3)
+
+
+                .build());
+
+
+
+
+
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_DECODE_JUICE_DARK)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
+    }
+}
