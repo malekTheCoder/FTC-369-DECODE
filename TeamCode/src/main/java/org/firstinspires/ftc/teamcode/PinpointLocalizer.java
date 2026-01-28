f4158105775fe10abedc0b3ab53807aff2ae6913
Malek Swilam <malekgswilam@gmail.com>
2026-01-28 10:31:44 -0500

    fixed drift issue, applied new yaw scalar of 1.0006

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PinpointLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PinpointLocalizer.java
@@ -34,7 +34,10 @@ public final class PinpointLocalizer implements Localizer {
         driver = hardwareMap.get(GoBildaPinpointDriver.class, "pinpoint");
 
         double mmPerTick = inPerTick * 25.4;
-        driver.setEncoderResolution(1 / mmPerTick, DistanceUnit.MM);
+        //driver.setEncoderResolution(1 / mmPerTick, DistanceUnit.MM);
+        driver.setEncoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD);
+        driver.setYawScalar(1.0006);
+
         // rr calculation for offsets using angular ramp logger
         //driver.setOffsets(mmPerTick * PARAMS.parYTicks, mmPerTick * PARAMS.perpXTicks, DistanceUnit.MM);
 
@@ -43,7 +46,8 @@ public final class PinpointLocalizer implements Localizer {
         // driver.setOffsets(6.16023622, 0.0523622, DistanceUnit.INCH);
 
         //OFFSETS FOR MIDDLE OF DRIVETRAIN (AXIS OF ROTATION) IN MM
-        driver.setOffsets(156.865139, 35.134861, DistanceUnit.MM);
+        driver.setOffsets(156, 12, DistanceUnit.MM);
+
 
 
 
@@ -51,11 +55,16 @@ public final class PinpointLocalizer implements Localizer {
         initialParDirection = GoBildaPinpointDriver.EncoderDirection.FORWARD;
         initialPerpDirection = GoBildaPinpointDriver.EncoderDirection.REVERSED;
 
+//        System.out.print(driver.getYawScalar());
         driver.setEncoderDirections(initialParDirection, initialPerpDirection);
 
         driver.resetPosAndIMU();
 
         txWorldPinpoint = initialPose;
+    }
+
+    public double printYawScalar(){
+        return driver.getYawScalar();
     }
 
     @Override
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele/TestRRLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele/TestRRLocalizer.java
@@ -13,7 +13,16 @@ import org.firstinspires.ftc.teamcode.Subsystems.Drivetrain;
 public class TestRRLocalizer extends OpMode {
     MecanumDrive mecanumDrive;
     Drivetrain drive;
-     Pose2d startPose = new Pose2d(72,-72, Math.toRadians(180));
+//     Pose2d startPose = new Pose2d(72,-72, Math.toRadians(180));
+   Pose2d startPose = new Pose2d(0,0, Math.toRadians(0));
+
+    double lastHeadingDeg = 0;
+    double fullHeading = 0;
+    boolean firstLoop = true;
+
+    // FACTORY YAW SCALAR: 1.0031
+    //NEW ATTEMPTED YAW SCALAR 1.0006
+
 
 
     //Pose2d startPose = new Pose2d(-59,-47, 234);
@@ -32,19 +41,39 @@ public class TestRRLocalizer extends OpMode {
         Pose2d currentPose = mecanumDrive.localizer.getPose();
         double heading = Math.toDegrees(mecanumDrive.localizer.getPose().heading.toDouble());
 
+        if (firstLoop) {
+            lastHeadingDeg = heading;
+            firstLoop = false;
+        }
+        double delta = heading - lastHeadingDeg;
+        if (delta > 180) delta -= 360;
+        if (delta < -180) delta += 360;
+        fullHeading += delta;
+        lastHeadingDeg = heading;
+
         double inverseHeading = (360 + Math.toDegrees(mecanumDrive.localizer.getPose().heading.toDouble())) % 360;
 
 
         drive.handleDrivetrain(gamepad1);
 
+        double yawScalar = mecanumDrive.localizer.printYawScalar();
+
+
+
         if (gamepad1.x){
             drive.resetIMU();
+            fullHeading = 0;
+            firstLoop = true;
+            lastHeadingDeg = heading;
         }
 
 
         telemetry.addData("X", currentPose.position.x);
         telemetry.addData("Y", currentPose.position.y);
+        telemetry.addData("full heading", fullHeading);
         telemetry.addData("inverse heading (degrees)", inverseHeading);
+        telemetry.addData("yaw scalar", yawScalar);
+
 
 
 
